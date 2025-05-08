7f906aba488e122c7d80791a2fa6201222d45bb2
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-08 18:41:47 -0300

    feat: criar página de login com autenticação de usuário

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useAuth } from "@/contexts/AuthContext";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "@/lib/firebase";
+import { useRouter } from "next/router";
+import withAuth from "@/hooks/withAuth";
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const { user } = useAuth();
+    const router = useRouter();
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            setError("Email ou senha inválidos!");
+        }
+    };
+
+    if (user) {
+        router.push("/"); // Redireciona para a página inicial se o usuário já estiver logado
+        return null;
+    }
+
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h1 className="text-2x1 font-bold mb-6 text-center">Login</h1>
+                {error && <p className="text-red-500 mb-4">{error}</p>}
+
+                <form onSubmit={handleLogin}>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 mb-2">E-mail</label>
+                        <input
+                            type="email" value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 mb-2">Senha</label>
+                        <input
+                            type="password" value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="w-full px-3 py-2 border rounded"
+                            required
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-200"
+                    >
+                        Entrar
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+
+}
+
+export default withAuth([])(Login); // Permite acesso a todos os usuários, mesmo não autenticados
