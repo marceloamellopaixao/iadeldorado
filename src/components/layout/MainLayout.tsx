594c074a76820c769f58819377ed3090f06070c2
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-08 18:39:08 -0300

    feat: adicionar componente MainLayout para estruturação da página com autenticação e navegação

---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,34 @@
+// src/components/layout/MainLayout.tsx
+import { useAuth } from '@/contexts/AuthContext'
+import UserProfile from '@/components/common/UserProfile'
+import ButtonRouter from '@/components/common/ButtonRouter'
+
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+    const { user } = useAuth()
+
+    return (
+        <div className="min-h-screen flex flex-col">
+            <header className="bg-blue-600 text-white p-4">
+                <div className="container mx-auto flex justify-between items-center">
+                    <h1 className="text-xl font-bold"><a href="/">IAD Eldorado - Cantina</a></h1>
+                    {!user && (
+                        <div className="flex space-x-4">
+                            <ButtonRouter
+                                nameButton="Entrar"
+                                color="bg-blue-500 text-white font-bold px-4 py-2 rounded hover:bg-blue-300 hover:text-black transition duration-300"
+                                rota="/auth/login"
+                            />
+                            <ButtonRouter
+                                nameButton="Registrar"
+                                color="bg-blue-500 text-white font-bold px-4 py-2 rounded hover:bg-blue-300 hover:text-black transition duration-300"
+                                rota="/auth/register"
+                            />
+                        </div>
+                    )}
+                    {user && <UserProfile />}
+                </div>
+            </header>
+            <main className="flex-grow container mx-auto p-4">{children}</main>
+        </div>
+    )
+}
