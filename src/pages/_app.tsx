a652b20c9963bf09cd92f8cea8007b404407ed85
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-08 18:40:25 -0300

    feat: envolver o componente principal com AuthProvider e MainLayout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
+import { AuthProvider } from "@/contexts/AuthContext";
+import MainLayout from "@/components/layout/MainLayout";
 import type { AppProps } from "next/app";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
+    </AuthProvider>
+  );
 }
