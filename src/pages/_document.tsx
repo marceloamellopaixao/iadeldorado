d9becf5a2485a1551c8d07d611e5934ade126f44
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-08 18:40:33 -0300

    feat: atualizar idioma do documento para pt-BR e organizar metadados

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="pt-BR">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="theme-color" content="#ff0000" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className="bg-gray-50">
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
