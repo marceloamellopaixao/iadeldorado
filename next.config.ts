c04052dda4691b881b8f9aff95f8a394a6897c41
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-08 18:36:00 -0300

    chore: update dependencies and add new packages
    - Added date-fns, firebase, react-chartjs-2, react-firebase-hooks, and react-icons to dependencies.
    - Updated devDependencies: added @eslint/eslintrc and updated typescript.
    - Removed duplicate entry for next in dependencies.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  allowedDevOrigins: process.env.NODE_ENV === 'development' ? undefined : [
+    "http://localhost:3000",
+    "http://26.219.51.63:3000",
+    "http://192.168.15.2:3000"
+  ]
 };
 
 export default nextConfig;
