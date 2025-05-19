71fec16866b8a6d38ef487a23e755bdea6aa62cf
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-19 12:41:13 -0300

    feat: adicionar funcionalidade de mostrar/ocultar senha e link para recuperação de senha

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { useAuth } from "@/contexts/AuthContext";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "@/lib/firebase";
 import { useRouter } from "next/router";
 import withAuth from "@/hooks/withAuth";
+import Image from "next/image";
+import eyeIcon from "@/assets/icons/eye-solid.svg";
+import eyeCloseIcon from "@/assets/icons/eye-slash-solid.svg";
+import Link from "next/link";
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
     const [error, setError] = useState("");
     const { user } = useAuth();
     const router = useRouter();
@@ -18,6 +24,14 @@ function Login() {
             await signInWithEmailAndPassword(auth, email, password);
         } catch {
             setError("Email ou senha inválidos!");
+        }
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+
+        if (passwordInputRef.current) {
+            passwordInputRef.current.type = showPassword ? "text" : "password";
         }
     };
 
@@ -44,12 +58,26 @@ function Login() {
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-700 mb-2">Senha</label>
-                        <input
-                            type="password" value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="w-full text-gray-700 px-3 py-2 border-3 border-blue-500 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            required
-                        />
+                        <div className="relative ">
+                            <input
+                                ref={passwordInputRef}
+                                type={showPassword ? "text" : "password"} value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="w-full text-gray-700 px-3 py-2 border-3 border-blue-500 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" type="button" onClick={togglePasswordVisibility}>
+                                {showPassword ? (
+                                    <Image src={eyeCloseIcon} alt="Ocultar senha" width={20} height={20} />
+                                ) : (
+                                    <Image src={eyeIcon} alt="Mostrar senha" width={20} height={20} />
+
+                                )}
+                            </button>
+                        </div>
+                        <Link href="/auth/forgot-password" className="text-gray-600 text-sm">
+                            Esqueceu sua senha?
+                        </Link>
                     </div>
                     <button
                         type="submit"
