a91e441ad3d528dbd59003cd3ea9b4f5c04d5670
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-19 12:40:52 -0300

    refactor: simplificar lógica de redirecionamento e remover carregamento desnecessário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,22 @@
-import LoadingSpinner from '@/components/common/LoadingSpinner'
 import { useAuth } from '@/contexts/AuthContext'
 import { useRouter } from 'next/router'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 export default function Home() {
   const { user, userData } = useAuth()
-  const [loading, setLoading] = useState(true)
   const router = useRouter()
 
   useEffect(() => {
-    if (user) {
-      router.push(userData?.role === 'admin' ? '/admin/products' : '/products')
+    if (userData?.role === 'admin') {
+      router.push('/admin/products')
+    } else if (userData?.role === 'customer') {
+      router.push('/products')
+    } else if (userData?.role === 'seller') {
+      router.push('/seller/orders')
     } else {
       router.push('/products')
     }
   }, [user, userData, router])
-
-  useEffect(() => {
-    // Simula um carregamento de dados
-    const timer = setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, [])
-
-  if (loading) {
-    return <LoadingSpinner message='Carregando...' />
-  }
 
   return null;
 }
