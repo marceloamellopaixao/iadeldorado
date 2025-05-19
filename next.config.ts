c59479abafd110812151c08d1a14fe32a40e0bc5
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-18 21:48:22 -0300

    feat: adicionar configurações de exportação e otimização de imagens no next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
   allowedDevOrigins: process.env.NODE_ENV === 'development' ? undefined : [
     "http://localhost:3000",
     "http://26.219.51.63:3000",
