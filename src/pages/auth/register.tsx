19228a900b390719c0ebfedb6874bb06edaaa8fd
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-19 12:41:18 -0300

    feat: adicionar funcionalidade de mostrar/ocultar senha no registro

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,13 +1,18 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import { auth, db } from "@/lib/firebase";
 import { useRouter } from "next/router";
 import withAuth from "@/hooks/withAuth";
+import Image from "next/image";
+import eyeIcon from "@/assets/icons/eye-solid.svg";
+import eyeCloseIcon from "@/assets/icons/eye-slash-solid.svg";
 
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
+    const passwordInputRef = useRef<HTMLInputElement>(null);
     const [name, setName] = useState("");
     const [telephone, setTelephone] = useState("");
     const [error, setError] = useState("");
@@ -30,6 +35,14 @@ function Register() {
 
         } catch (error) {
             setError(error instanceof Error ? error.message : "Erro ao criar conta");
+        }
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+
+        if (passwordInputRef.current) {
+            passwordInputRef.current.type = showPassword ? "text" : "password";
         }
     };
 
@@ -68,12 +81,23 @@ function Register() {
                     </div>
                     <div className="mb-6">
                         <label className="block text-gray-700 mb-2">Senha</label>
-                        <input
-                            type="password" value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="w-full text-gray-700 px-3 py-2 border-3 rounded border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            required
-                        />
+                        <div className="relative ">
+                            <input
+                                ref={passwordInputRef}
+                                type={showPassword ? "text" : "password"} value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="w-full text-gray-700 px-3 py-2 border-3 border-blue-500 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                required
+                            />
+                            <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" type="button" onClick={togglePasswordVisibility}>
+                                {showPassword ? (
+                                    <Image src={eyeCloseIcon} alt="Ocultar senha" width={20} height={20} />
+                                ) : (
+                                    <Image src={eyeIcon} alt="Mostrar senha" width={20} height={20} />
+
+                                )}
+                            </button>
+                        </div>
                     </div>
                     <button
                         type="submit"
