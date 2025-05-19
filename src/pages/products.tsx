33f797d27d1d02585498ad8c04e850a6839512e8
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-18 21:48:46 -0300

    refactor: remover importação não utilizada do useEffect no componente ProductsPage

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useCart } from '@/hooks/useCart';
 import { ProductCard } from '@/components/common/ProductCard';
 import { withAuth } from '@/hooks/withAuth';
