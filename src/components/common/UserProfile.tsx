9432a5b6b8c0eede2946ddb98ba2516549cd68ed
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-20 16:08:35 -0300

    refactor: melhorar a estrutura do componente UserProfile para melhor legibilidade e responsividade

--- src/components/common/UserProfile.tsx
+++ src/components/common/UserProfile.tsx
@@ -1,6 +1,8 @@
 import { useAuth } from '@/contexts/AuthContext'
 import ButtonRouter from '@/components/common/ButtonRouter'
 import LogoutButton from '@/components/common/LogoutButton'
+import Image from 'next/image'
+import userIcon from '@/assets/icons/user-solid.svg'
 
 export default function UserProfile() {
     const { userData } = useAuth()
@@ -8,17 +10,19 @@ export default function UserProfile() {
 
     return (
         <div className='flex items-center gap-4'>
-            <div className='text-right'>
+            <div className='text-right hidden md:flex flex-col'>
                 <p className='text-sm font-semibold'>{userData?.name}</p>
                 <p className='text-xs text-white-500'>
                     {userData?.role === 'admin' ? 'Administrador' : userData?.role === 'seller' ? 'Vendedor' : 'Cliente'}
                 </p>
             </div>
             <ButtonRouter
-                nameButton="Perfil"
-                color="bg-blue-500 text-white font-bold px-4 py-2 rounded hover:bg-blue-300 hover:text-black transition duration-300"
+                color="flex flex-row gap-2 bg-blue-500 text-white font-bold px-4 py-2 rounded hover:bg-blue-800 transition duration-300"
                 rota="/auth/profile"
-            />
+            >
+                <Image src={userIcon} alt="User Icon" width={20} height={20} />
+                Perfil
+            </ButtonRouter>
             <LogoutButton />
         </div>
     )
