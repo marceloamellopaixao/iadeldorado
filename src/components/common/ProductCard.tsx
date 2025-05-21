83d7fe434d39d0b12fa80224e1932931aa7743c4
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-20 21:14:25 -0300

    feat: adicionar verificação de estoque no botão de adicionar ao carrinho no componente ProductCard

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -15,12 +15,15 @@ export function ProductCard({ product, onAddToCart, isInCart }: ProductCardProps
             <p className="font-bold text-lg">R$ {product.price.toFixed(2).replace('.', ',')}</p>
             <button
                 onClick={onAddToCart}
-                className={`w-full mt-4 py-2 rounded ${isInCart
-                    ? 'bg-green-500 text-white font-bold'
-                    : 'bg-blue-500 text-white font-bold hover:bg-blue-600'
+                disabled={product.stock <= 0}
+                className={`w-full mt-4 py-2 rounded ${product.stock <= 0
+                    ? 'bg-gray-400 text-white cursor-not-allowed'
+                    : isInCart
+                        ? 'bg-green-500 text-white font-bold'
+                        : 'bg-blue-500 text-white font-bold hover:bg-blue-600'
                     }`}
             >
-                {isInCart ? '✔ No Carrinho' : 'Adicionar'}
+                {product.stock <= 0 ? 'Sem Estoque' : isInCart ? '✔ No Carrinho' : 'Adicionar'}
             </button>
         </div>
     );
