7a6ae94afd271d40aeb6a9f36187ea7d7017f3f9
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-23 19:11:07 -0300

    feat: remover meta viewport do documento

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="theme-color" content="#ff0000" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="icon" href="/logo-adeldorado.png" />
       </Head>
