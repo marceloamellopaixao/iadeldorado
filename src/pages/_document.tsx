1213e7883e0b3a7cbc76a63a8bf37692a188a109
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-05-23 19:04:10 -0300

    feat: atualizar ícone do site e garantir responsividade com meta viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,8 +7,9 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="theme-color" content="#ff0000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo-adeldorado.png" />
       </Head>
       <body className="bg-gray-50">
         <Main />
