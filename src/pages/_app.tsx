4e513a508dd937c1857eb1f3138dd7424e1d0f49
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-07-06 17:07:17 -0300

    feat: adicionar ToastContainer para exibir notificações no layout principal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import { AuthProvider } from "@/contexts/AuthContext";
 import MainLayout from "@/components/layout/MainLayout";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <MainLayout>
         <Component {...pageProps} />
+        <ToastContainer />
       </MainLayout>
     </AuthProvider>
   );
