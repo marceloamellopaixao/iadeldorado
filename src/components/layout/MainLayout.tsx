7f9827e01579054dc5786a4fe596c25c90e80038
marceloamellopaixao <marceloamellopaixao@gmail.com>
2025-07-06 21:36:14 -0300

    feat: atualizar o layout principal para utilizar classes do Tailwind CSS, garantindo uma estrutura flexível e responsiva

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import Header from './Header'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Header />
             <main>
                 {children}
